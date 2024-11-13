44a990623500c5f278e7ff401fb1ffa562746f45
elj40 <elaijoubert@gmail.com>
2024-11-13 08:53:40 +0200

    Star patterns start

---
+++ Star_Patterns/main.c
@@ -0,0 +1,20 @@
+/*
+* Filename:	main.c
+* Date:		13/11/2024 
+* Name:		EL Joubert
+*
+* Print out a grid of stars in various different patterns
+* Take input from the user to figure out how big the grid must be
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	// Step 1: How to print out one line of stars, length specified by user?
+	// Step 2: How could we make patterns in the one line? e.g. alternating space and *
+	// Step 3: How do we repeat the printing of one line to draw a grid?
+	// Step 4: How can we do different patterns in the grid?
+    printf("Hello world!\n");
+    return 0;
+}
